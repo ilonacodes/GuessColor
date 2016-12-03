b5da2f5fde8f7e4400966b271087ba778ef5c2bf
Ilona Demidenko <demiluri@gmail.com>
2016-12-03 13:07:15 +0100

    Add tests to checkUserGuess

--- spec/GuessColorSpec.js
+++ spec/GuessColorSpec.js
@@ -1,7 +1,53 @@
 describe("guess color", function () {
+    var storedMessage;
     it("is unknown message when user enters unknown color ", function () {
-        window.prompt = function () {
-            return "qwert";
-        }
-    }); 
+        window.alert = function (message) {
+          storedMessage = message;
+        };
+
+        var result = checkUserGuess("qwerty", 3, 0);
+
+        expect(storedMessage).toEqual("I do not recognize that color!");
+        expect(result).toEqual(true);
+    });
+
+    it("is message when user enters color that alphabetically higher than computer's color", function () {
+        window.alert = function (message) {
+            storedMessage = message;
+        };
+
+        var result = checkUserGuess("black", 2, 0);
+
+        expect(storedMessage).toEqual("Your input alphabetically higher than mine!");
+        expect(result).toEqual(true);
+    });
+
+    it("is message when user enters color that alphabetically lower than computer's color", function () {
+        window.alert = function (message) {
+            storedMessage = message;
+        };
+
+        var result = checkUserGuess("green", 4, 0);
+
+        expect(storedMessage).toEqual("Your input alphabetically lower than mine!");
+        expect(result).toEqual(true);
+    });
+
+    it("is message when user enters color that alphabetically lower than computer's color", function () {
+        window.alert = function (message) {
+            storedMessage = message;
+        };
+
+        var element = {style: {background: ""}};
+        window.query = function (tagName) {
+            return element;
+        };
+
+        var result = checkUserGuess("green", 1, 42);
+
+        expect(storedMessage).toEqual("You are right! You took " + 42 + " guesses!");
+        expect(element.style.background).toEqual("green");
+        expect(result).toEqual(false);
+    });
+
 });
--- src/GuessColor.js
+++ src/GuessColor.js
@@ -1,19 +1,22 @@
 var colors = ["red", "green", "blue", "white", "black", "yellow"];
-var targetColor;
-var userInput;
-var countIteration = 0;
 
 function game() {
+    var targetColor;
+    var userInput;
+    var countIteration = 0;
     colors.sort();
     targetColor = Math.floor(Math.random() * colors.length);
     do {
         userInput = prompt("I am thinking of one of colors\n\n" + colors + "\n\nWhat color am I thinking of?");
         countIteration++;
-    } while (checkUserGuess());
-    countIteration = 0;
+    } while (checkUserGuess(userInput, targetColor, countIteration));
 }
 
-function checkUserGuess() {
+function query(tagName) {
+    return document.getElementsByTagName(tagName)[0];
+}
+
+function checkUserGuess(userInput, targetColor, countIteration) {
     var index = colors.indexOf(userInput);
     if (index < 0) {
         alert("I do not recognize that color!");
@@ -22,7 +25,7 @@ function checkUserGuess() {
     } else if (index < targetColor) {
         alert("Your input alphabetically lower than mine!");
     } else {
-        inputElement = document.getElementsByTagName("body")[0];
+        var inputElement = query("body");
         inputElement.style.background = colors[targetColor];
         alert("You are right! You took " + countIteration + " guesses!");
         return false;
